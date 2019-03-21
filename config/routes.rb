b149681429ee62cffe856b137499e93be47d7272
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-03-21 19:59:53 +0000

    Adds new user registration routes and forms

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  resources :user
+  get '/user/new' => 'user#new'
+  post '/user/new' => 'user#create'
+
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
