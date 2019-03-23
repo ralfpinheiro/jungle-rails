90b6cdc5fbf2901bece06a3340c3b89fd5c75722
Ralf Pinheiro <pinheiro.ralf@gmail.com>
2019-03-23 00:35:42 +0000

    Adds review form submission functionality and redirects to product page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,28 @@
 class ReviewsController < ApplicationController
+
+
     def create
-        raise "Yay, I'm here!"
+    current_user = User.find(session[:user_id]) if session[:user_id]
+    product = Product.find params[:product_id]
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.product_id = product.id
+
+    if @review.valid?
+      @review.save
+    end
+      redirect_to product
+ end
+
+      private
+    
+      def review_params
+        params.require(:review).permit(
+          :rating,
+          :description
+        )
       end
 
 end
+
+
